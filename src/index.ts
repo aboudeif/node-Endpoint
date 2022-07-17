72bc271b5798df8228d343923ebc6a7259f2c4e8
Abdallah Aboudeif <71098171+aboudeif@users.noreply.github.com>
2022-07-17 06:11:16 +0200

    read img :)

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express from 'express';
+import express, { response } from 'express';
 import path from 'path';
+import fs from "fs";
+import { request } from 'http';
+
 const app = express();
-const port = 5500;
+const port = 5700;
 
-// check for port to avoid allready in use error testing
-app.listen(port, () => {
 
+// check for port to avoid in use error testing
+
+app.get('/students', (req, res) => {
+
+    res.sendFile(path.join(__dirname, '../public/img/logo.jpg'));
 });
-app.get('/', (req, res) => {
-    res.render(__dirname + 'html/index.html');
-    
-});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
 
 export default app;
